5b12235e058f1e99cebdc7a738842fec0ced8d36
FabriceLing <fabricel@bittitan.com>
2018-05-07 16:28:04 -0700

    Add 3 scripts for Josh.

---
+++ Get-DevConsole/GetDevConsole.ps1
@@ -0,0 +1,23 @@
+# Set strict mode.
+Set-StrictMode -Version 2.0
+
+# Disable progress bar.
+$ProgressPreference ="SilentlyContinue"
+
+# Set default ErrorAction to Stop.
+$ErrorActionPreference = "Stop"
+
+#Import-Module
+Import-Module ".\GetDevConsole.psm1" -Force -DisableNameChecking
+
+# URL of the console tool.
+$URL = "https://btdevconsole.azurewebsites.net"
+
+# Get the authentication cookie.
+$cookies = Get-DevConsoleCookies $URL
+
+# Get the name of the lastest DevConsole file.
+$filename = Get-LastestDevConsoleFilename $URL $cookies
+
+# Download the lastest DevConsole file.
+Download-DevConsole $URL $filename $cookies
